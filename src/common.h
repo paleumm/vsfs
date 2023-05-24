ccf6956c1573520aa0d77019430fc35c7fcb5f79
paleumm <zdotqdot@gmail.com>
2023-05-24 10:26:05 +0000

    Update common

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+typedef enum {_DIR, _FILE, _FREE} ftype;
+typedef unsigned int _u32;
+typedef unsigned short _u16;
+typedef unsigned char _u8;
